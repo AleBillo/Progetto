2de81aaf32ca28decded1a5543c17c7a78e1ffc7
Mactar Seck <mactaribrahima.seck@studio.unibo.it>
2024-12-06 20:57:31 +0100

    Aggiornato database

--- billo_vinils_sql.sql
+++ billo_vinils_sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 06, 2024 alle 10:52
+-- Creato il: Dic 06, 2024 alle 20:56
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -81,80 +81,25 @@ CREATE TABLE `vinyls` (
   `artist` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `year` int(11) NOT NULL,
-  `category_id` int(11) DEFAULT NULL
+  `category_id` int(11) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `vinyls`
 --
 
-INSERT INTO `vinyls` (`id_vinyl`, `vinyl_name`, `artist`, `price`, `year`, `category_id`) VALUES
-(1, 'The Dark Side of the Moon', 'Pink Floyd', 35.50, 1973, 1),
-(2, 'To Pimp a Butterfly', 'Kendrick Lamar', 25.99, 2015, 2),
-(3, 'A Night at the Opera', 'Queen', 30.00, 1975, 1),
-(4, 'DAMN.', 'Kendrick Lamar', 26.99, 2017, 2),
-(5, 'Kind of Blue', 'Miles Davis', 28.50, 1959, 4),
-(6, 'Ready to Die', 'The Notorious B.I.G.', 29.50, 1994, 2),
-(7, 'Abbey Road', 'The Beatles', 29.99, 1969, 1),
-(8, 'The College Dropout', 'Kanye West', 27.50, 2004, 3),
-(9, 'Illmatic', 'Nas', 24.99, 1994, 2),
-(10, 'Take Five', 'Dave Brubeck', 22.00, 1959, 4);
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_category`);
-
---
--- Indici per le tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indici per le tabelle `vinyls`
---
-ALTER TABLE `vinyls`
-  ADD PRIMARY KEY (`id_vinyl`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT per la tabella `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT per la tabella `vinyls`
---
-ALTER TABLE `vinyls`
-  MODIFY `id_vinyl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `vinyls`
---
-ALTER TABLE `vinyls`
-  ADD CONSTRAINT `vinyls_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id_category`);
+INSERT INTO `vinyls` (`id_vinyl`, `vinyl_name`, `artist`, `price`, `year`, `category_id`, `image_url`) VALUES
+(1, 'The Dark Side of the Moon', 'Pink Floyd', 35.50, 1973, 1, 'The_dark_side_of_the_moon.jpg'),
+(2, 'To Pimp a Butterfly', 'Kendrick Lamar', 25.99, 2015, 2, 'To_pimp_a_butterfly.jpg'),
+(3, 'A Night at the Opera', 'Queen', 30.00, 1975, 1, 'A_night_at_the_opera.jpg'),
+(4, 'DAMN.', 'Kendrick Lamar', 26.99, 2017, 2, 'DAMN.jpg'),
+(5, 'Kind of Blue', 'Miles Davis', 28.50, 1959, 4, 'Kind_of_blue.jpg'),
+(6, 'Ready to Die', 'The Notorious B.I.G.', 29.50, 1994, 2, 'Ready_to_die.jpg'),
+(7, 'Abbey Road', 'The Beatles', 29.99, 1969, 1, 'Abbey_road.jpg'),
+(8, 'The College Dropout', 'Kanye West', 27.50, 2004, 3, 'The_college_dropout.jpg'),
+(9, 'Illmatic', 'Nas', 24.99, 1994, 2, 'Illmatic.jpg'),
+(10, 'Take Five', 'Dave Brubeck', 22.00, 1959, 4, 'Take_five.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
