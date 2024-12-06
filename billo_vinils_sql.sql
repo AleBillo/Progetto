e1bbcefad250478f09ccc2b7a9144b8ed76556b3
Mactar Seck <mactaribrahima.seck@studio.unibo.it>
2024-12-06 19:17:28 +0100

    modifica

--- billo_vinils_sql.sql
+++ billo_vinils_sql.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `billo_vinils_database`
+-- Database: `billo_vinils_sql`
 --
 
 -- --------------------------------------------------------
