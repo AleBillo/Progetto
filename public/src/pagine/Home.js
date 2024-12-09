c9ba04047458be05629156ff250a4aa47cf9c133
AleBillo <alessandro.dominici2@studio.unibo.it>
2024-12-09 15:32:56 +0100

    color changing

--- public/src/pagine/Home.js
+++ public/src/pagine/Home.js
@@ -2,15 +2,15 @@ const Home = {
     template:
         `
 
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
-      <img src="./media/LOGO.png" style="width: 450px;" alt="Logo">
+      <img src="./media/favicon.png" style="width: 450px;" alt="Logo">
       </div>
     <div>
 
-    <div class="col-md-12 border border-2">
-    <div class="boxindex rounded text-center text-white overflow-hidden mx-auto">
-        <div class="mb-3 py-3">
+    <div class="col-md-12 ">
+    <div class="indexbg rounded text-center text-white overflow-hidden mx-auto">
+        <div class="mb-3">
             <h2 class="display-5">"Il suono del vinile, l'emozione di sempre."</h2>
             <p class="lead">Benvenuti nel nostro sito dedicato agli amanti del vinile, dove la passione per la musica autentica prende vita. Ogni disco che trovi qui è selezionato con cura, dai classici intramontabili alle edizioni rare e ricercate. Che tu sia un collezionista esperto o un appassionato alle prime armi, ti offriamo una vasta gamma di vinili di alta qualità, insieme ad accessori e consigli per la cura della tua collezione. Qui, la musica è un viaggio, un'emozione che si riscopre a ogni ascolto. Scopri il fascino senza tempo del vinile con noi.</p>
         </div>
