983781e6b4235c5ed8bc49a74c8ce7c94ba05562
AleBillo <alessandro.dominici2@studio.unibo.it>
2024-12-09 14:59:26 +0100

    Creazione Login estetica

--- public/src/pagine/login.js
+++ public/src/pagine/login.js
@@ -1,6 +1,21 @@
 export default {
   template: `
 
+    <div class="login-container container-fluid">
+    <form class="login-form">
+      <label for="email">EMAIL</label>
+      <input type="email" id="email" placeholder="Inserisci la tua email" required />
+      
+      <label for="password">PASSWORD</label>
+      <input class="input" type="password" id="password" placeholder="Inserisci la tua password" required />
+      
+      <button type="submit" class="btn-login">ACCEDI</button>
+      
+      <p class="separator">Oppure</p>
+      
+      <button type="button" class="btn-register">REGISTRATI</button>
+    </form>
+  </div>
    
   `,
   
